1a57b21886c5c9fad6ccf107123242ed4eb3c2e7
nickbloo <nickbloo2@gmail.com>
2022-11-09 14:57:06 -0600

    next commit recent

--- api/index.js
+++ api/index.js
@@ -4,6 +4,38 @@ const postsRouter = require("./posts");
 const tagsRouter = require("./tags");
 const apiRouter = express.Router();
 
+const { getUserById } = require("../db");
+const { JWT_SECRET } = process.env;
+const jwt = require("jsonwebtoken");
+
+apiRouter.use(async (req, res, next) => {
+    const prefix = "Bearer ";
+    const auth = req.header("Authorization");
+
+    if (!auth) {
+        console.log("No token!")
+        next();
+    } else if (auth.startsWith(prefix)) {
+        const token = auth.slice(prefix.length);
+
+        try {
+            const { id } = jwt.verify(token, JWT_SECRET);
+
+            if (id) {
+                req.user = await getUserById(id);
+                next();
+            }
+        } catch ({name, message}) {
+            next({name, message});
+        }
+    } else {
+        next({
+            name: "AuthorizationHeaderError",
+            message: `Authorization token must start with ${prefix}`
+        });
+    }
+});
+
 apiRouter.use("/users", usersRouter);
 apiRouter.use("/posts", postsRouter);
 apiRouter.use("/tags", tagsRouter);
--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const morgan = require("morgan");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const {JWT_SECRET } = process.env;
 const { client } = require("./db")
 
 app.use(morgan("dev"));
@@ -21,44 +20,6 @@ app.use((req, res, next) => {
 });
 
 client.connect();
-
-// async function decryptJWT(req, res, next) {
-//     try {
-//         const authHeader= req.headers.authorization
-//         if (!authHeader) {
-//             res.error("Invalid token from headers")
-//         } else {
-//             const slicedToken = authHeader.slice(7);
-//             const verifiedAuthData = jwt.verify(slicedToken, JWT_SECRET);
-
-//             console.log("Verified auth data: ", verifiedAuthData)
-//         }
-//         next();
-//     } catch (error) {
-//         console.error
-//     }
-// };
-
-// app.use(decryptJWT);
-
-// app.get("/", (req, res, next) => {
-//     res.send(`<h1>Hello World!</h1>`)
-// });
-
-// app.post("/register", (req, res, next) => {
-//     try {
-//         let newUserData = req.body;
-
-//         const newToken = jwt.sign({
-//             username: newUserData.username
-//         }, JWT_SECRET, {
-//             expiresIn: "1w"
-//         })
-//         res.end()
-//     } catch (error) {
-//         console.error
-//     }
-// })
 
 const PORT = 3000
 
